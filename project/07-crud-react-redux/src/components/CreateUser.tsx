42c6990c906f72b16419b17141b0dab6fafc4234
Mauricio Vélez <serketh7@gmail.com>
2023-06-07 15:23:03 -0500

    form controled

--- project/07-crud-react-redux/src/components/CreateUser.tsx
+++ project/07-crud-react-redux/src/components/CreateUser.tsx
@@ -1,6 +1,27 @@
 import { Button, Card, Text, TextInput } from '@tremor/react';
+import { useState } from 'react';
 
 export default function CreateUser() {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    github: '',
+  });
+
+  const handleonChange = (e) => {
+    e.preventDefault();
+    setUser((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <Card
       style={{
@@ -19,11 +40,33 @@ export default function CreateUser() {
           flexDirection: 'column',
           margin: '8px',
         }}
+        onSubmit={handleSubmit}
       >
-        <TextInput name='username' placeholder='Name...' />
-        <TextInput name='email' placeholder='mail@mail.com' />
-        <TextInput name='github' placeholder='github' />
-        <Button type='submit' style={{ marginTop: '16px' }}>
+        <TextInput
+          name='username'
+          value={user.username}
+          onChange={handleonChange}
+          placeholder='Name...'
+        />
+        <TextInput
+          name='email'
+          value={user.email}
+          onChange={handleonChange}
+          placeholder='mail@mail.com'
+        />
+        <TextInput
+          name='github'
+          value={user.github}
+          onChange={handleonChange}
+          placeholder='github'
+        />
+        <Button
+          type='submit'
+          style={{ marginTop: '16px' }}
+          onClick={() => {
+            handleSubmit;
+          }}
+        >
           Create
         </Button>
       </form>
