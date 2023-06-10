664171de6d064d581708e9beb09d8f87b1c66c79
Mauricio Vélez <serketh7@gmail.com>
2023-06-10 13:23:25 -0500

    Restore all users

--- project/08-prueba-tech-react-typescript/src/App.tsx
+++ project/08-prueba-tech-react-typescript/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { type Users } from '../src/types.ts';
 import './App.css';
 import { ListOfUsers } from './components/ListOfUsers.tsx';
@@ -8,14 +8,20 @@ function App() {
   const [users, setUsers] = useState<Users[]>([]);
   const [showColors, setShowColors] = useState(false);
   const [sortedByCountry, setSortByCountry] = useState(false);
+  const originalUsers = useRef<Users[]>([]);
 
   const sortByCountry = () => {
     setSortByCountry(!sortedByCountry);
   };
+  console.log(sortedByCountry);
+  
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
       .then((res) => res.json())
-      .then((data) => setUsers(data.results));
+      .then((data) => {
+        setUsers(data.results);
+        originalUsers.current = data.results;
+      });
   }, []);
 
   const togglePaint = () => {
@@ -28,16 +34,23 @@ function App() {
       })
     : users;
 
-  const handleDelete = (mail: string ) => {
+  const handleDelete = (mail: string) => {
     const newUsers = users.filter((user) => user.email != mail);
     setUsers(newUsers);
-  }
+  };
 
-  
+  const restoreAllUsers = () => {
+    setUsers(originalUsers.current);
+  };
+
   return (
     <>
       <h1>Prueba Tecnica</h1>
-      <UserActions togglePaint={togglePaint} sortByCountry={sortByCountry} />
+      <UserActions
+        togglePaint={togglePaint}
+        sortByCountry={sortByCountry}
+        restoreAllUsers={restoreAllUsers}
+      />
       <ListOfUsers
         sorted={sorted}
         showColors={showColors}
--- project/08-prueba-tech-react-typescript/src/components/UserActions.tsx
+++ project/08-prueba-tech-react-typescript/src/components/UserActions.tsx
@@ -1,15 +1,19 @@
 interface Props {
   togglePaint: () => void;
   sortByCountry: () => void;
+  restoreAllUsers: () => void;
 }
 
-export function UserActions({ togglePaint, sortByCountry }: Props) {
+export function UserActions({
+  togglePaint,
+  sortByCountry,
+  restoreAllUsers,
+}: Props) {
   return (
     <header style={{ marginBottom: '32px' }}>
-      <button onClick={togglePaint} style={{ margin: '4px' }}>
-        Paint files
-      </button>
+      <button onClick={togglePaint}>Paint files</button>
       <button onClick={sortByCountry}>Sort by Country</button>
+      <button onClick={restoreAllUsers}>Restore Users</button>
     </header>
   );
 }
